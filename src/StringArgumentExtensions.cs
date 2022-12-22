3a01c59c6d473ff36d9040003c2d2199182dae53
davit-asryan-sveasolar <100680564+davit-asryan-sveasolar@users.noreply.github.com>
2022-12-22 23:58:06 +0400

    Add MatchesRegex argument validator

--- src/StringArgumentExtensions.cs
+++ src/StringArgumentExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.Extensions.Args
 {
@@ -51,6 +52,31 @@ namespace Microsoft.Extensions.Args
 			if (string.IsNullOrWhiteSpace(argument.Value))
 			{
 				throw new ArgumentException(string.Format(message, argument.Name));
+			}
+			return argument;
+		}
+
+		/// <summary>
+		/// Validate that the argument matches the specified regex
+		/// </summary>
+		/// <param name="argument">The string argument to validate</param>
+		/// <param name="regex">Regex pattern</param>
+		[DebuggerStepThrough]
+		public static Argument<string> MatchesRegex(this Argument<string> argument, string regex) => 
+			argument.IsNotNullOrWhitespace("String argument '{0}' should match the following pattern: {1}, but it doesn't.");
+
+		/// <summary>
+		/// Validate that the argument matches the specified regex
+		/// </summary>
+		/// <param name="argument">The string argument to validate</param>
+		/// <param name="regex">Regex pattern</param>
+		/// <param name="message">Exception message template</param>
+		[DebuggerStepThrough]
+		public static Argument<string> MatchesRegex(this Argument<string> argument, string regex, string message)
+		{
+			if (Regex.IsMatch(argument.Value, regex))
+			{
+				throw new ArgumentException(string.Format(message, argument.Name, regex));
 			}
 			return argument;
 		}
